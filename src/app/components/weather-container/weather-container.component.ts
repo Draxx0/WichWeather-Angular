f5e1d2c0801d01fd2504d49bd43962ba8c14412b
Draxx0 <williamfort.lmgl@gmail.com>
2022-02-02 17:03:13 +0100

    dataUpdate

--- src/app/components/weather-container/weather-container.component.ts
+++ src/app/components/weather-container/weather-container.component.ts
@@ -10,7 +10,15 @@ export class WeatherContainerComponent implements OnInit {
   cityName: string = '';
   cityWeather: string = '';
   cityTemp: string = '';
-  backgroundImgUrl : string = '../../../assets/img/bg/Clouds.jpg'
+  weatherHumidity: string = '';
+  weatherPressure: string = '';
+  weatherFeelsLike: string = '';
+  weatherWindSpeed: string = '';
+  weatherTempMax: string = '';
+  weatherTempMin: string = '';
+  weatherSunrise: number = 0;
+  weatherSunset: number = 0;
+  backgroundImgUrl: string = '../../../assets/img/bg/Clouds.jpg';
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
@@ -18,32 +26,48 @@ export class WeatherContainerComponent implements OnInit {
       let lat = position.coords.latitude;
       let long = position.coords.longitude;
       this.httpClient
-        .get<any>(`http://localhost:3000/test?lat=${lat}&lon=${long}&units=metric&appid=d916bb262d7dc0b62e3bd99b9bc40bd0`)
-        .subscribe((data) => { 
+        .get<any>(
+          `http://localhost:3000/test?lat=${lat}&lon=${long}&units=metric&appid=d916bb262d7dc0b62e3bd99b9bc40bd0`
+        )
+        .subscribe((data) => {
           console.log(data);
-          this.cityName = data.cityName
-          this.cityWeather = data.weatherName
-          this.cityTemp = data.weatherTemp
+          this.cityName = data.cityName;
+          this.cityWeather = data.weatherName;
+          this.cityTemp = data.weatherTemp;
+          this.weatherHumidity = data.weatherHumidity;
+          this.weatherPressure = data.weatherPressure;
+          this.weatherFeelsLike = data.weatherFeelsLike;
+          this.weatherWindSpeed = data.weatherWindSpeed;
+          this.weatherTempMax = data.weatherTempMax;
+          this.weatherTempMin = data.weatherTempMin;
+          this.weatherSunrise = data.weatherSunrise*1000;
+          this.weatherSunset = data.weatherSunset*1000;
+          console.log(this.weatherSunset);
+          
         });
     });
-
-    
-  };
-
-
+  }
 
   loadParisName(): void {
     this.httpClient
       .get<any>('http://localhost:3000?cityName=paris')
       .subscribe((data) => {
         console.log(data);
-        this.cityName = data.cityName
-        this.cityWeather = data.weatherName
-        this.cityTemp = data.weatherTemp
+        this.cityName = data.cityName;
+        this.cityWeather = data.weatherName;
+        this.cityTemp = data.weatherTemp;
+        this.weatherHumidity = data.weatherHumidity;
+        this.weatherPressure = data.weatherPressure;
+        this.weatherWindSpeed = data.weatherWindSpeed;
+        this.weatherTempMax = data.weatherTempMax;
+        this.weatherTempMin = data.weatherTempMin;
+        this.weatherSunrise = data.weatherSunrise*1000;
+        this.weatherSunset = data.weatherSunset*1000;
+        this.weatherFeelsLike = data.weatherFeelsLike;
 
         // if (data.weather.Name == "cloudy") {
         //   document.body.style.backgroundImage = "https://via.placeholder.com"
-        // } 
+        // }
       });
   }
 
@@ -52,9 +76,18 @@ export class WeatherContainerComponent implements OnInit {
       .get<any>('http://localhost:3000?cityName=bordeaux')
       .subscribe((data) => {
         console.log(data);
-        this.cityName = data.cityName
-        this.cityWeather = data.weatherName
-        this.cityTemp = data.weatherTemp
+        this.cityName = data.cityName;
+        this.cityWeather = data.weatherName;
+        this.cityTemp = data.weatherTemp;
+        this.weatherHumidity = data.weatherHumidity;
+        this.weatherPressure = data.weatherPressure;
+        this.weatherWindSpeed = data.weatherWindSpeed;
+        this.weatherTempMax = data.weatherTempMax;
+        this.weatherTempMin = data.weatherTempMin;
+        this.weatherSunrise = data.weatherSunrise*1000;
+        this.weatherSunset = data.weatherSunset*1000;
+        this.weatherFeelsLike = data.weatherFeelsLike;
+        
       });
   }
 
@@ -63,55 +96,93 @@ export class WeatherContainerComponent implements OnInit {
       .get<any>('http://localhost:3000?cityName=strasbourg')
       .subscribe((data) => {
         console.log(data);
-        this.cityName = data.cityName
-        this.cityWeather = data.weatherName
-        this.cityTemp = data.weatherTemp
+        this.cityName = data.cityName;
+        this.cityWeather = data.weatherName;
+        this.cityTemp = data.weatherTemp;
+        this.weatherHumidity = data.weatherHumidity;
+        this.weatherPressure = data.weatherPressure;
+        this.weatherWindSpeed = data.weatherWindSpeed;
+        this.weatherTempMax = data.weatherTempMax;
+        this.weatherFeelsLike = data.weatherFeelsLike;
+        this.weatherTempMin = data.weatherTempMin;
+        this.weatherSunrise = data.weatherSunrise*1000;
+        this.weatherSunset = data.weatherSunset*1000;
       });
   }
 
+  loadToulouseName(): void {
+    this.httpClient
+      .get<any>('http://localhost:3000?cityName=toulouse')
+      .subscribe((data) => {
+        console.log(data);
+        this.cityName = data.cityName;
+        this.cityWeather = data.weatherName;
+        this.cityTemp = data.weatherTemp;
+        this.weatherHumidity = data.weatherHumidity;
+        this.weatherPressure = data.weatherPressure;
+        this.weatherWindSpeed = data.weatherWindSpeed;
+        this.weatherTempMax = data.weatherTempMax;
+        this.weatherTempMin = data.weatherTempMin;
+        this.weatherSunrise = data.weatherSunrise*1000;
+        this.weatherSunset = data.weatherSunset*1000;
+        this.weatherFeelsLike = data.weatherFeelsLike;
+      });
+  }
 
-loadToulouseName(): void {
-  this.httpClient
-    .get<any>('http://localhost:3000?cityName=toulouse')
-    .subscribe((data) => {
-      console.log(data);
-      this.cityName = data.cityName
-      this.cityWeather = data.weatherName
-      this.cityTemp = data.weatherTemp
-    });
-}
+  loadLilleName(): void {
+    this.httpClient
+      .get<any>('http://localhost:3000?cityName=lille')
+      .subscribe((data) => {
+        console.log(data);
+        this.cityName = data.cityName;
+        this.cityWeather = data.weatherName;
+        this.cityTemp = data.weatherTemp;
+        this.weatherHumidity = data.weatherHumidity;
+        this.weatherPressure = data.weatherPressure;
+        this.weatherWindSpeed = data.weatherWindSpeed;
+        this.weatherTempMax = data.weatherTempMax;
+        this.weatherTempMin = data.weatherTempMin;
+        this.weatherSunrise = data.weatherSunrise*1000;
+        this.weatherFeelsLike = data.weatherFeelsLike;
+        this.weatherSunset = data.weatherSunset*1000;
+      });
+  }
 
+  loadBrestName(): void {
+    this.httpClient
+      .get<any>('http://localhost:3000?cityName=brest')
+      .subscribe((data) => {
+        console.log(data);
+        this.cityName = data.cityName;
+        this.cityWeather = data.weatherName;
+        this.cityTemp = data.weatherTemp;
+        this.weatherHumidity = data.weatherHumidity;
+        this.weatherPressure = data.weatherPressure;
+        this.weatherWindSpeed = data.weatherWindSpeed;
+        this.weatherFeelsLike = data.weatherFeelsLike;
+        this.weatherTempMax = data.weatherTempMax;
+        this.weatherTempMin = data.weatherTempMin;
+        this.weatherSunrise = data.weatherSunrise*1000;
+        this.weatherSunset = data.weatherSunset*1000;
+      });
+  }
 
-loadOrleansName(): void {
-  this.httpClient
-    .get<any>('http://localhost:3000?cityName=orleans')
-    .subscribe((data) => {
-      console.log(data);
-      this.cityName = data.cityName
-      this.cityWeather = data.weatherName
-      this.cityTemp = data.weatherTemp
-    });
-}
-
-loadBrestName(): void {
-  this.httpClient
-    .get<any>('http://localhost:3000?cityName=brest')
-    .subscribe((data) => {
-      console.log(data);
-      this.cityName = data.cityName
-      this.cityWeather = data.weatherName
-      this.cityTemp = data.weatherTemp
-    });
-}
-
-loadMontpellierName(): void {
-  this.httpClient
-    .get<any>('http://localhost:3000?cityName=montpellier')
-    .subscribe((data) => {
-      console.log(data);
-      this.cityName = data.cityName
-      this.cityWeather = data.weatherName
-      this.cityTemp = data.weatherTemp
-    });
-}
+  loadMontpellierName(): void {
+    this.httpClient
+      .get<any>('http://localhost:3000?cityName=montpellier')
+      .subscribe((data) => {
+        console.log(data);
+        this.cityName = data.cityName;
+        this.cityWeather = data.weatherName;
+        this.cityTemp = data.weatherTemp;
+        this.weatherHumidity = data.weatherHumidity;
+        this.weatherPressure = data.weatherPressure;
+        this.weatherWindSpeed = data.weatherWindSpeed;
+        this.weatherFeelsLike = data.weatherFeelsLike;
+        this.weatherTempMax = data.weatherTempMax;
+        this.weatherTempMin = data.weatherTempMin;
+        this.weatherSunrise = data.weatherSunrise*1000;
+        this.weatherSunset = data.weatherSunset*1000;
+      });
+  }
 }
